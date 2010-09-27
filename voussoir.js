09090c07546fe297279d8bb40e6937a53bf1db0f
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2010-09-26 22:51:39 -0400

    accept json strings, and print them back out

--- voussoir.js
+++ voussoir.js
@@ -1,6 +1,7 @@
 /*jslint indent:2, browser:true, devel:true, maxlen:80 nomen:false */
 /*global $ require */
 var sys = require('sys'),
+url = require('url'),
 http = require('http');
 
 var server = http.createServer(function (request, response) {
@@ -14,12 +15,14 @@ var server = http.createServer(function (request, response) {
       "things, they don't", request.method, "them!\n"
     ].join(' '));
   } else {
+    var post_data = "";
     request.on('data', function (chunk) {
-      sys.puts(chunk);
+      post_data += chunk;
     });
-
     request.on('end', function () {
+      var data = JSON.parse(post_data);
       response.writeHead(202);
+      sys.puts(JSON.stringify(data));
       response.end();
     });
   }
