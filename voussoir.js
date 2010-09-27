98a6bac41aca3a593eb0c17481400db8f9bb9d21
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2010-09-27 06:05:31 -0400

    first barely functional iso generation stuff done.

--- voussoir.js
+++ voussoir.js
@@ -5,22 +5,26 @@ url = require('url'),
 fs = require('fs'),
 http = require('http'),
 path = require('path'),
+events = require('events'),
+spawn = require('child_process').spawn,
 archiso_base = path.join(__dirname, 'archiso'),
+copy_tracker = new events.EventEmitter(),
 copy_directory = function (the_path, dest_path) {
   fs.readdir(the_path, function (err, files) {
+    copy_tracker.emit('start_dir', files.length);
     files.forEach(function (file) {
       var the_file = path.join(the_path, file);
       fs.stat(the_file, function (err, stats) {
         var dest_file = path.join(dest_path, file);
         if (stats.isDirectory()) {
           fs.mkdir(dest_file, 0777, function (err) {
+            copy_tracker.emit('written');
             copy_directory(the_file, dest_file);
           });
         } else {
           fs.readFile(the_file, function (err, data) {
-            sys.puts(dest_file);
             fs.writeFile(dest_file, data, function (err) {
-
+              copy_tracker.emit('written');
             });
           });
         }
@@ -44,14 +48,44 @@ server = http.createServer(function (request, response) {
       post_data += chunk;
     });
     request.on('end', function () {
-      var data = JSON.parse(post_data),
+      var user_data = JSON.parse(post_data),
       date = new Date(),
-      working_dir = [data.name, '-', date.getTime()].join('');
+      working_dir = [user_data.name, '-', date.getTime()].join('');
 
       response.writeHead(202);
-      sys.puts(JSON.stringify(data));
+      sys.puts(JSON.stringify(user_data));
       response.end();
       fs.mkdir(working_dir, 0777, function (err) {
+        var make_dir = path.join(working_dir, 'configs/syslinux-iso'),
+        num_files = 0,
+        done_files = 0;
+        copy_tracker.on('start_dir', function (num) {
+          num_files += num;
+        });
+        copy_tracker.on('written', function () {
+          done_files += 1;
+          if (done_files === num_files) {
+            fs.writeFile(
+              path.join(make_dir, 'packages.' + user_data.arch),
+              user_data.packages.join('\n'),
+              function (err) {
+                var maker = spawn(
+                  'make', [],
+                  {'cwd': fs.realpathSync(make_dir)}
+                );
+                maker.on('exit', function (code) {
+                  sys.puts('finished with code: ' + code);
+                });
+                maker.stdout.on('data', function (data) {
+                  sys.puts(user_data.name + ' ' + data);
+                });
+                maker.stderr.on('data', function (data) {
+                  sys.puts(user_data.name + ' ERR: ' + data);
+                });
+              }
+            );
+          }
+        });
         copy_directory(archiso_base, working_dir);
       });
     });
